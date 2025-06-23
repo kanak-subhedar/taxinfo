98fba5b53cfa7192dca204ac043509b84a15a372
kanak-subhedar <162018874+kanak-subhedar@users.noreply.github.com>
2025-06-23 15:36:51 +0530

    Update app-magnet.py

--- app-magnet.py
+++ app-magnet.py
@@ -18,6 +18,7 @@ client = razorpay.Client(auth=(RAZORPAY_KEY, RAZORPAY_SECRET))
 def verify_and_download():
     data = request.json
     payment_id = data.get('payment_id')
+    print(f"🧾 Received payment_id: {payment_id}")
 
     try:
         payment = client.payment.fetch(payment_id)
