952a4db049d4983b64dd958ef37dd7ecb3d1bb53
kanak-subhedar <162018874+kanak-subhedar@users.noreply.github.com>
2025-11-07 20:57:13 +0530

    Update app-magnet.py

--- app-magnet.py
+++ app-magnet.py
@@ -5,6 +5,11 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Client Magnet backend is running successfully!"})
+
 # Razorpay credentials (from Render environment variables)
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
