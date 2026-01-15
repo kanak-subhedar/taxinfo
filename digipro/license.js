1e8b8c8419e352200d7b4c9eea4fceb303995e6a
kanak-subhedar <162018874+kanak-subhedar@users.noreply.github.com>
2026-01-15 07:24:40 +0530

    Update license.js

--- digipro/license.js
+++ digipro/license.js
@@ -1,1 +1,18 @@
-// license code for charts & graphs
+(function () {
+  const allowedDomains = [
+    "t24k.com",
+    "www.t24k.com"
+  ];
+
+  const currentDomain = location.hostname;
+
+  if (!allowedDomains.includes(currentDomain)) {
+    document.body.innerHTML = `
+      <h2 style="color:red;text-align:center">
+        Unauthorized domain<br>
+        This tool works only on t24k.com
+      </h2>
+    `;
+    throw new Error("Unauthorized domain");
+  }
+})();
