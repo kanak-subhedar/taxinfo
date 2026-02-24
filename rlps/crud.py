368d0040ec79be99d43ea6497683d17efa3c9cc0
kanak-subhedar <162018874+kanak-subhedar@users.noreply.github.com>
2026-02-24 23:01:52 +0530

    Update crud.py

--- rlps/crud.py
+++ rlps/crud.py
@@ -1,1 +1,21 @@
+from sqlalchemy.orm import Session
+from models import Quotation
+from schemas import QuotationCreate
 
+def create_quotation(db: Session, quotation: QuotationCreate):
+    db_quotation = Quotation(**quotation.dict())
+    db.add(db_quotation)
+    db.commit()
+    db.refresh(db_quotation)
+    return db_quotation
+
+def get_all_quotations(db: Session):
+    return db.query(Quotation).all()
+
+def get_pending_followups(db: Session):
+    from datetime import date
+    today = date.today()
+    return db.query(Quotation).filter(
+        Quotation.followup_date <= today,
+        Quotation.status == "Open"
+    ).all()
